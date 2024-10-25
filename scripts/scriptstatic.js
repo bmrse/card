76f0dae4246116314d4c1f6a56a7ec98a7a7b8d0
bmrse <gta69editz@gmail.com>
2024-10-25 19:12:14 +0530

    Update scriptstatic.js

--- scripts/scriptstatic.js
+++ scripts/scriptstatic.js
@@ -1,4 +1,4 @@
-const userID = "738748102311280681"; // Thay đổi thành ID Discord của bạn
+const userID = "887557388700368896"; // Thay đổi thành ID Discord của bạn
 
 const elements = {
 	statusBox: document.getElementById("status"),

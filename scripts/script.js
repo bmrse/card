610c0f4117c7a0e0a65435a689d9544c74e7b39b
bmrse <gta69editz@gmail.com>
2024-10-25 13:56:24 +0530

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-const userID = "738748102311280681"; // Change this to your Discord user ID
+const userID = "887557388700368896"; // Change this to your Discord user ID
 
 const elements = {
 	statusBox: document.getElementById("status"),
